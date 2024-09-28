a2ed469aa57102e2cc247dcfa900a0f083dba447
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-28 23:36:05 +0900

    feat: #2 삭제 상태를 나타내는 `DeletedStatus` Enum 생성

---
+++ src/main/java/com/leets/xcellentbe/domain/shared/DeletedStatus.java
@@ -0,0 +1,13 @@
+package com.leets.xcellentbe.domain.shared;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum DeletedStatus {
+	DELETED("DELETED"),
+	NOT_DELETED("NOT_DELETED");
+
+	private final String userStatus;
+}
