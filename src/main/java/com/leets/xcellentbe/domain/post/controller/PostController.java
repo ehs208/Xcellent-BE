f23645b86d2d1dc71aec02bdae6f4f72854183eb
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-28 23:49:21 +0900

    feat: #2 `Post` 컨트롤러 생성

---
+++ src/main/java/com/leets/xcellentbe/domain/post/controller/PostController.java
@@ -0,0 +1,12 @@
+package com.leets.xcellentbe.domain.post.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
+public class PostController {
+}
