86322001ce1af7e4f37553d67acb9ee888325899
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-28 23:48:00 +0900

    feat: #2 `Hashtag` 도메인 생성

---
+++ src/main/java/com/leets/xcellentbe/domain/hashtag/domain/Hashtag.java
@@ -0,0 +1,25 @@
+package com.leets.xcellentbe.domain.hashtag.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hashtag {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long HashtagId;
+
+	@NotNull
+	@Column(length = 30)
+	private String content;
+}
