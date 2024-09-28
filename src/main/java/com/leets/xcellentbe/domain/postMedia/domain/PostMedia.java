0454d0feb7d78671f99b98434a4c1d32c1cfb1b1
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-29 00:45:55 +0900

    fix: #2 PostMedia ID 생성 시 발생한 오류 수정

--- src/main/java/com/leets/xcellentbe/domain/postMedia/domain/PostMedia.java
+++ src/main/java/com/leets/xcellentbe/domain/postMedia/domain/PostMedia.java
@@ -24,7 +24,7 @@ import lombok.NoArgsConstructor;
 public class PostMedia extends BaseTimeEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID PostMediaId;
 
 	@NotNull
