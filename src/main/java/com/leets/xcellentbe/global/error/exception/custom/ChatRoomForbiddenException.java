19f3ab82f2fa41ed8b0d0e25e9bb4256c06a280e
Dongyun Kim <djskaldrjfj@naver.com>
2024-10-04 15:05:24 +0900

    fix: 에러코드 및 검색예외 제거

--- src/main/java/com/leets/xcellentbe/global/error/exception/custom/ChatRoomForbiddenException.java
+++ src/main/java/com/leets/xcellentbe/global/error/exception/custom/ChatRoomForbiddenException.java
@@ -3,8 +3,9 @@ package com.leets.xcellentbe.global.error.exception.custom;
 import com.leets.xcellentbe.global.error.ErrorCode;
 import com.leets.xcellentbe.global.error.exception.CommonException;
 
-public class ChatRoomUnauthorizedException extends CommonException {
-	public ChatRoomUnauthorizedException() {
-		super(ErrorCode.CHAT_ROOM_UNAUTHORIZED);
+public class ChatRoomForbiddenException extends CommonException {
+	public ChatRoomForbiddenException() {
+
+		super(ErrorCode.CHAT_ROOM_FORBIDDEN);
 	}
 }
--- src/main/java/com/leets/xcellentbe/global/error/exception/custom/FailedSearchException.java
+++
@@ -1,8 +0,0 @@
-package com.leets.xcellentbe.global.error.exception.custom;
-
-import com.leets.xcellentbe.global.error.ErrorCode;
-import com.leets.xcellentbe.global.error.exception.CommonException;
-
-public class FailedSearchException extends CommonException {
-	public FailedSearchException() { super(ErrorCode.FAILED_SEARCH);}
-}
