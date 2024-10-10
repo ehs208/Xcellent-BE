654e9a1aa0a697ad8058d49a8d8260f59f75a39d
EunHyunsu <ehs208@naver.com>
2024-10-10 22:37:44 +0900

    refactor: #7 디렉토리 구조 수정
    인증 및 인가에 관한 컨트롤러를 따로 생성하여 분리하였습니다.

--- src/main/java/com/leets/xcellentbe/domain/user/controller/UserController.java
+++ src/main/java/com/leets/xcellentbe/domain/user/controller/UserController.java
@@ -16,22 +16,22 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
-	private final UserService userService;
-
-	@PostMapping("/auth/register")
-	@Operation(summary = "회원가입", description = "회원가입을 합니다.")
-	public ResponseEntity<GlobalResponseDto<String>> register(@RequestBody UserSignUpRequestDto userSignUpRequestDto) {
-		return ResponseEntity.status(HttpStatus.CREATED)
-			.body(GlobalResponseDto.success(userService.register(userSignUpRequestDto), HttpStatus.CREATED.value()));
-	}
-
-	@Operation(summary = "로그인", description = "사용자의 이메일과 비밀번호로 로그인합니다.")
-	@PostMapping("/auth/login")
-	public String login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
-		// 로그인 로직 처리
-		return "로그인 성공";
-	}
+	// private final UserService userService;
+	//
+	// @PostMapping("/auth/register")
+	// @Operation(summary = "회원가입", description = "회원가입을 합니다.")
+	// public ResponseEntity<GlobalResponseDto<String>> register(@RequestBody UserSignUpRequestDto userSignUpRequestDto) {
+	// 	return ResponseEntity.status(HttpStatus.CREATED)
+	// 		.body(GlobalResponseDto.success(userService.register(userSignUpRequestDto), HttpStatus.CREATED.value()));
+	// }
+	//
+	// @Operation(summary = "로그인", description = "사용자의 이메일과 비밀번호로 로그인합니다.")
+	// @PostMapping("/auth/login")
+	// public String login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+	// 	// 로그인 로직 처리
+	// 	return "로그인 성공";
+	// }
 }
