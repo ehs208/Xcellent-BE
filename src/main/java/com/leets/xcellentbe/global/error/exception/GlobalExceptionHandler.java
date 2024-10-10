1abfed79321e7bc710338bbf5c218c315ae07082
EunHyunsu <ehs208@naver.com>
2024-10-10 22:58:59 +0900

    feat: #7 예외 발생시 스택 트레이스 출력

--- src/main/java/com/leets/xcellentbe/global/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/leets/xcellentbe/global/error/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.leets.xcellentbe.global.error.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.leets.xcellentbe.global.error.*;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -13,6 +15,7 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleCommonException(CommonException ex) {
 		ErrorCode errorCode = ex.getErrorCode(); // 전달된 예외에서 에러 코드 가져오기
 		ErrorResponse errorResponse = new ErrorResponse(errorCode);
+		showErrorLog(errorCode);
 		return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatus()));
 	}
 
@@ -20,7 +23,13 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
 		ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
 		ErrorResponse errorResponse = new ErrorResponse(errorCode);
+		showErrorLog(errorCode);
 		return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+
+	private static void showErrorLog(ErrorCode errorCode) {
+		log.error("errorCode: {}, message: {}", errorCode.getCode(), errorCode.getMessage());
 	}
 
 }
