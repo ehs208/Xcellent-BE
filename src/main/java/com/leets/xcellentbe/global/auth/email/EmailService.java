af9365da613af59d6b383178079fe3bf5aa05709
EunHyunsu <ehs208@naver.com>
2024-10-15 20:49:31 +0900

    fix: #9 이메일 전송 관련 예외처리 추가

--- src/main/java/com/leets/xcellentbe/global/auth/email/EmailService.java
+++ src/main/java/com/leets/xcellentbe/global/auth/email/EmailService.java
@@ -48,13 +48,18 @@ public class EmailService {
 			throw new AuthCodeAlreadySentException();
 		}
 
+	try {
 		MimeMessage message = mailSender.createMimeMessage();//JavaMailSender 객체를 사용하여 MimeMessage 객체를 생성
-		MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");// true를 전달하여 multipart 형식의 메시지를 지원하고, "utf-8"을 전달하여 문자 인코딩을 설정
-		helper.setTo(toMail);//이메일의 수신자 주소 설정
+		MimeMessageHelper helper = new MimeMessageHelper(message, true,
+			"utf-8");// true를 전달하여 multipart 형식의 메시지를 지원하고, "utf-8"을 전달하여 문자 인코딩을 설정
+		helper.setTo("h");//이메일의 수신자 주소 설정
 		helper.setSubject(title);//이메일의 제목을 설정
-		helper.setText(content,true);//이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정으로한다.
+		helper.setText(content, true);//이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정으로한다.
 		mailSender.send(message);
-
+	}
+	catch(Exception e) {
+		throw new EmailCannotBeSent();
+	}
 		redisService.setDataExpire(toMail,Integer.toString(authNumber));
 	}
 
