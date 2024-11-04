40cbae46d4cab85ef8ededf10708145932ea5c3e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 12:23:07 +0900

    feat: #17 Redis 메시지 퍼블리셔 서비스 추가
    - DMDto를 사용하여 주어진 채널 토픽에 메시지 발행 기능 구현

---
+++ src/main/java/com/leets/xcellentbe/global/redis/RedisPublisher.java
@@ -0,0 +1,19 @@
+package com.leets.xcellentbe.global.redis;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+import com.leets.xcellentbe.domain.dm.dto.DMDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RedisPublisher {
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public void publish(ChannelTopic topic, DMDto dmDto) {
+		redisTemplate.convertAndSend(topic.getTopic(), dmDto);
+	}
+}
