cb8b8c57f1fe5e687376298447ce87dee620c3a6
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 12:32:31 +0900

    feat: #17 DM 엔티티 수정 및 기능 추가
    - DMId 필드를 BINARY(16)로 설정하여 UUID 저장 형식 변경
    - ChatRoom과의 관계를 업데이트하여 chatRoom_id를 사용하도록 수정
    - DM 전송자(sender)와 수신자(receiver) 추가
    - DeletedStatus 필드를 EnumType.STRING으로 설정
    - DM 객체 생성 메서드(create) 추가
    - DM 생성자에 Builder 패턴 적용
    - 삭제 메서드(delete) 추가하여 메시지 상태 변경 기능 구현

--- src/main/java/com/leets/xcellentbe/domain/dm/domain/DM.java
+++ src/main/java/com/leets/xcellentbe/domain/dm/domain/DM.java
@@ -2,13 +2,15 @@ package com.leets.xcellentbe.domain.dm.domain;
 
 import java.util.UUID;
 
-import com.leets.xcellentbe.domain.chatroom.domain.Chatroom;
+import com.leets.xcellentbe.domain.chatRoom.domain.ChatRoom;
 import com.leets.xcellentbe.domain.shared.BaseTimeEntity;
 import com.leets.xcellentbe.domain.shared.DeletedStatus;
 import com.leets.xcellentbe.domain.user.domain.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +19,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,17 +30,22 @@ public class DM extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(columnDefinition = "BINARY(16)")
 	private UUID DMId;
 
 	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "chatroom_id")
-	private Chatroom chatRoom;
+	@JoinColumn(name = "chatRoom_id")
+	private ChatRoom chatRoom;
 
 	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "participant_id")
-	private User participant;
+	@JoinColumn(name = "sender_id")
+	private User sender;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "receiver_id")
+	private User receiver;
 
 	@NotNull
 	@Column
@@ -45,5 +53,29 @@ public class DM extends BaseTimeEntity {
 
 	@NotNull
 	@Column
+	@Enumerated(EnumType.STRING)
 	private DeletedStatus deletedStatus;
+
+	public static DM create(User sender, User receiver, ChatRoom chatRoom, String message) {
+		return DM.builder()
+			.sender(sender)
+			.receiver(receiver)
+			.chatRoom(chatRoom)
+			.message(message)
+			.build();
+	}
+
+	@Builder
+	private DM(User sender, User receiver, ChatRoom chatRoom, String message) {
+		super();
+		this.sender = sender;
+		this.receiver = receiver;
+		this.chatRoom = chatRoom;
+		this.message = message;
+		this.deletedStatus = DeletedStatus.NOT_DELETED;
+	}
+
+	public void delete() {
+		this.deletedStatus = DeletedStatus.DELETED;
+	}
 }
