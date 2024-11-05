061a7ef79228f1228030d7f31e5b31a621f2a350
Dongyun Kim <djskaldrjfj@naver.com>
2024-11-05 13:56:48 +0900

    feat: #16 모든 게시글 조회 기능 추가

--- src/main/java/com/leets/xcellentbe/domain/article/domain/repository/ArticleRepository.java
+++ src/main/java/com/leets/xcellentbe/domain/article/domain/repository/ArticleRepository.java
@@ -1,26 +1,16 @@
 package com.leets.xcellentbe.domain.article.domain.repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.leets.xcellentbe.domain.article.domain.Article;
-import com.leets.xcellentbe.domain.shared.DeletedStatus;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, UUID> {
-
-	Optional<Article> findByWriterId(Long writerId);
-
-	Optional<Article> findByWriterIdAndIsPinned(Long writerId, boolean isPinned);
-
-	Optional<Article> findByDeleteStatus(DeletedStatus deletedStatus);
-
-	Optional<Article> findByHashTag(String hashTag);
-
-	Optional<Article> findByRePost(String rePost);
-
-	//리스트(목록) 찾으려면 Page활용 / 커서 페이징 찾기
+	//메인 페이지 모든 게시글 조회
+	Page<Article> findAll(Pageable pageable);
 }
