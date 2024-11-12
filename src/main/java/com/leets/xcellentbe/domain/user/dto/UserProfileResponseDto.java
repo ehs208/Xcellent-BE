839a8b10a7406ebdf20c14c73da9e2cd5e9c69d9
EunHyunsu <ehs208@naver.com>
2024-11-12 11:14:22 +0900

    feat: #22 유저 정보에 데이터 추가

--- src/main/java/com/leets/xcellentbe/domain/user/dto/UserProfileResponseDto.java
+++ src/main/java/com/leets/xcellentbe/domain/user/dto/UserProfileResponseDto.java
@@ -21,11 +21,16 @@ public class UserProfileResponseDto {
 	private int userBirthYear;
 	private int userBirthMonth;
 	private int userBirthDay;
+	private int followersCount;
+	private int followingsCount;
+	private boolean isFollowing;
+	private boolean isMyProfile;
 
 	@Builder
 	private UserProfileResponseDto(String email, String customId, String userName, String profileImageUrl,
 		String backgroundProfileImageUrl, String phoneNumber, String description, String websiteUrl, String location,
-		int userBirthYear, int userBirthMonth, int userBirthDay) {
+		int userBirthYear, int userBirthMonth, int userBirthDay, int followersCount, int followingsCount,
+		boolean isFollowing, boolean isMyProfile) {
 		this.email = email;
 		this.customId = customId;
 		this.userName = userName;
@@ -38,9 +43,14 @@ public class UserProfileResponseDto {
 		this.userBirthYear = userBirthYear;
 		this.userBirthMonth = userBirthMonth;
 		this.userBirthDay = userBirthDay;
+		this.followersCount = followersCount;
+		this.followingsCount = followingsCount;
+		this.isMyProfile = isMyProfile;
+		this.isFollowing = isFollowing;
 	}
 
-	public static UserProfileResponseDto from(User user) {
+	public static UserProfileResponseDto from(User user, int followersCount, int followingsCount, boolean isFollowing,
+		boolean isMyProfile) {
 		return UserProfileResponseDto.builder()
 			.email(user.getEmail())
 			.customId(user.getCustomId())
@@ -54,6 +64,10 @@ public class UserProfileResponseDto {
 			.userBirthYear(user.getUserBirth().getYear())
 			.userBirthMonth(user.getUserBirth().getMonth())
 			.userBirthDay(user.getUserBirth().getDay())
+			.followersCount(followersCount)
+			.followingsCount(followingsCount)
+			.isMyProfile(isMyProfile)
+			.isFollowing(isFollowing)
 			.build();
 	}
 }
